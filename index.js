3b7e00d4d78a097d39cfdba9fd8b8ce43f232597
Brad <bradarv90@gmail.com>
2016-08-13 21:12:55 -0400

    Adding toString for lower levels of node

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function (n) {
-  if (!toString.call(n).toLowerCase().includes('number')) {
+  if (!toString.call(n).toString().toLowerCase().includes('number')) {
     throw new TypeError('Passed in object is not a number', 'is-positive-number/index.js');
   }
   return n >= 0;
